5baf7ea3703d512e43fcb57c05e1e96d53d3c3de
Raju Dey <rd25aat@herts.ac.uk>
2026-04-13 23:53:18 +0100

    updated Review class

--- src/main/java/com/flc/booking/Review.java
+++ src/main/java/com/flc/booking/Review.java
@@ -9,5 +9,28 @@ package com.flc.booking;
  * @author ASUS
  */
 public class Review {
+    private Member member;
+    private int rating;
+    private String comment;
     
+    public Review(Member member, int rating, String comment){
+        if (rating < 1 || rating > 5){
+            throw new IllegalArgumentException("Rating must be between 1 and 5");
+        }
+        this.member = member;
+        this.rating = rating;
+        this.comment = comment;
+    }
+    
+    public int getRating(){
+        return rating;
+    }
+    
+    public String getComment(){
+        return comment;
+    }
+    
+    public Member getMember(){
+        return member;
+    }
 }
