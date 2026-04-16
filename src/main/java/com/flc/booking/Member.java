85ae5b1cccd6d015eb382cf52ecea62c742b168f
Raju Dey <rd25aat@herts.ac.uk>
2026-04-16 06:31:44 +0100

    Added member class functionality

--- src/main/java/com/flc/booking/Member.java
+++ src/main/java/com/flc/booking/Member.java
@@ -6,8 +6,69 @@ package com.flc.booking;
 
 /**
  *
- * @author ASUS
+ * @author rd25aat
  */
+import java.util.ArrayList;
+
 public class Member {
-    
+
+    private int memberId;
+    private String name;
+    private ArrayList<Booking> bookings;
+
+    public Member(int memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+        this.bookings = new ArrayList<>();
+    }
+
+    public boolean hasTimeConflict(Lesson lesson) {
+        for (Booking booking : bookings) {
+            Lesson bookedLesson = booking.getLesson();
+
+            if (bookedLesson.getWeekendNumber() == lesson.getWeekendNumber() &&
+                bookedLesson.getDay().equals(lesson.getDay()) &&
+                bookedLesson.getTimeSlot().equals(lesson.getTimeSlot())) {
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean bookLesson(Lesson lesson) {
+
+        if (lesson.isFull()) return false;
+
+        if (hasTimeConflict(lesson)) return false;
+
+        if (lesson.addMember(this)) {
+            bookings.add(new Booking(this, lesson));
+            return true;
+        }
+        return false;
+    }
+
+    public void cancelBooking(Lesson lesson) {
+
+        bookings.removeIf(b -> b.getLesson().equals(lesson));
+        lesson.removeMember(this);
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
+
+    @Override
+    public String toString() {
+        return memberId + " - " + name;
+    }
 }
